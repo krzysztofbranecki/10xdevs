179a997d7c7bf80c8b76e9d96afd6e5472b08243
krzysztofbranecki <krzysztof.branecki@frontkom.com>
2025-04-19 16:39:58 +0200

    chore: add config for depoy on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,15 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercel(),
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
-  }),
 });
